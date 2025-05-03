1c137f459a6e172add462f73c7ff5c537bf6ef4e
akongstad <kongstad@outlook.com>
2025-05-03 16:19:12 +0200

    chore: remove test deps

--- extension/opendic/opendic-impl/build.gradle.kts
+++ extension/opendic/opendic-impl/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation("org.apache.parquet:parquet-column:1.15.1")
     implementation("org.apache.parquet:parquet-common:1.15.1")
 
-
     implementation(libs.hadoop.common) {
         exclude("org.slf4j", "slf4j-reload4j")
         exclude("org.slf4j", "slf4j-log4j12")
@@ -109,38 +108,6 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     testImplementation(libs.logback.classic)
-
-    testFixturesImplementation(project(":polaris-core"))
-    testFixturesImplementation(project(":polaris-api-management-model"))
-    testFixturesImplementation(project(":polaris-api-management-service"))
-    testFixturesImplementation(project(":polaris-api-iceberg-service"))
-    testFixturesImplementation(project(":polaris-api-catalog-service"))
-
-    testFixturesImplementation(libs.jakarta.enterprise.cdi.api)
-    testFixturesImplementation(libs.jakarta.annotation.api)
-    testFixturesImplementation(libs.jakarta.ws.rs.api)
-
-    testFixturesImplementation(platform(libs.quarkus.bom))
-    testFixturesImplementation("io.quarkus:quarkus-rest-client")
-    testFixturesImplementation("io.quarkus:quarkus-rest-client-jackson")
-
-    testFixturesImplementation(platform(libs.iceberg.bom))
-    testFixturesImplementation("org.apache.iceberg:iceberg-api")
-    testFixturesImplementation("org.apache.iceberg:iceberg-core")
-    testFixturesImplementation("org.apache.iceberg:iceberg-aws")
-
-    testFixturesImplementation(platform(libs.google.cloud.storage.bom))
-    testFixturesImplementation("com.google.cloud:google-cloud-storage")
-    testFixturesImplementation(platform(libs.awssdk.bom))
-    testFixturesImplementation("software.amazon.awssdk:sts")
-    testFixturesImplementation("software.amazon.awssdk:iam-policy-builder")
-    testFixturesImplementation("software.amazon.awssdk:s3")
-
-    testFixturesImplementation(platform(libs.azuresdk.bom))
-    testFixturesImplementation("com.azure:azure-core")
-    testFixturesImplementation("com.azure:azure-storage-blob")
-    testFixturesImplementation("com.azure:azure-storage-file-datalake")
-
 
 }
 
